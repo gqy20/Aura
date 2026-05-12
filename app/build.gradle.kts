773775957717da060a6bcab95e442667b78f15c6
gqy20 <qingyu_ge@foxmail.com>
2026-05-12 14:52:11 +0800

    Enable Robolectric Android resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,12 @@ android {
         }
     }
 
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
